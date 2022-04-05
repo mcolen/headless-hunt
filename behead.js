3b77e6d8f5cb07610889868b0283d3bfda09ef95
mattcolen <mattcolen@gmail.com>
2022-04-05 16:58:02 -0700

    Upgrade manifest to v3

--- behead.js
+++ behead.js
@@ -1,10 +1,9 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.executeScript(
-    tabs[0].id,
-    {code: `
-      document.getElementsByClassName('pz-header')[0].style.display = 'none';
-    `});
-  chrome.pageAction.setTitle({'tabId': tabs[0].id, title: 'Re-head this page!'})
-  chrome.pageAction.setPopup({'tabId': tabs[0].id, popup: 'rehead.html'})
+  chrome.scripting.executeScript({
+    target: {tabId: tabs[0].id, allFrames: true},
+    func: () => {document.getElementsByClassName('pz-header')[0].style.display = 'none';},
+  });
+  chrome.action.setTitle({'tabId': tabs[0].id, title: 'Re-head this page!'})
+  chrome.action.setPopup({'tabId': tabs[0].id, popup: 'rehead.html'})
 });
 
--- rehead.js
+++ rehead.js
@@ -1,10 +1,9 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.executeScript(
-    tabs[0].id,
-    {code: `
-      document.getElementsByClassName('pz-header')[0].style.display = '';
-    `});
-  chrome.pageAction.setTitle({'tabId': tabs[0].id, title: 'Behead this page!'})
-  chrome.pageAction.setPopup({'tabId': tabs[0].id, popup: 'behead.html'})
+  chrome.scripting.executeScript({
+    target: {tabId: tabs[0].id, allFrames: true},
+    func: () => {document.getElementsByClassName('pz-header')[0].style.display = '';},
+  });
+  chrome.action.setTitle({'tabId': tabs[0].id, title: 'Behead this page!'})
+  chrome.action.setPopup({'tabId': tabs[0].id, popup: 'behead.html'})
 });
 
