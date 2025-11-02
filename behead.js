9129d8c0a6628d1ef220425d8ac4b89450aed646
Matt <github.uninsured181@passmail.net>
2025-11-01 23:52:49 -0700

    Remove legacy files - replaced by simplified toggle system

--- behead.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.scripting.executeScript({
-    target: {tabId: tabs[0].id, allFrames: true},
-    func: () => {document.getElementsByClassName('pz-header')[0].style.display = 'none';},
-  });
-  chrome.action.setTitle({'tabId': tabs[0].id, title: 'Re-head this page!'})
-  chrome.action.setPopup({'tabId': tabs[0].id, popup: 'rehead.html'})
-});
-
--- rehead.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.scripting.executeScript({
-    target: {tabId: tabs[0].id, allFrames: true},
-    func: () => {document.getElementsByClassName('pz-header')[0].style.display = '';},
-  });
-  chrome.action.setTitle({'tabId': tabs[0].id, title: 'Behead this page!'})
-  chrome.action.setPopup({'tabId': tabs[0].id, popup: 'behead.html'})
-});
-
